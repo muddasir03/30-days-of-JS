8719f8e091692e861355e186a22fc372ee387447
Muddasar Hussain <mudassarmanzoor03@gmail.com>
2023-07-07 11:20:05 +0500

    Time: 62 ms (65.49%), Space: 44.6 MB (50.04%) - LeetHub

---
+++ 2798-chunk-array/2798-chunk-array.js
@@ -0,0 +1,35 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    // Slice Solution.
+   const res=[];
+   for(let i=0;i<arr.length;i+=size)
+   {
+       res.push(arr.slice(i,i+size));
+   }
+   return res;
+};
+
+// Iterative Solution
+
+// let res=[];
+// let subarr=[];
+// for (let i=0;i<arr.length;i++)
+// {
+//     subarr.push(arr[i]);
+//     if(subarr.length===size)
+//     {
+//         res.push(subarr);
+//         subarr=[];
+//     }
+// }
+
+// if(subarr.length)
+// {
+//     res.push(subarr);
+// }
+
+// return res;
