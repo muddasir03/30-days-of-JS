b1c91919f10a6c0f5c04d7cbf0f5f9dbb89f5e79
Muddasar Hussain <mudassarmanzoor03@gmail.com>
2023-07-07 15:11:30 +0500

    Time: 102 ms (48.78%), Space: 52.7 MB (62.26%) - LeetHub

---
+++ 2804-compact-object/2804-compact-object.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Object} obj
+ * @return {Object}
+ */
+var compactObject = function(obj) {
+    // These three if statements deal with when obj is not an iterable object
+    if (obj === null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj !== "object") return obj;
+
+    // This for loop deals with when obj is an iterable object
+    const compacted = {};
+    for (const key in obj) {
+        let value = compactObject(obj[key]);
+        if (Boolean(value)) compacted[key] = value;
+    }
+
+    return compacted;
+};
